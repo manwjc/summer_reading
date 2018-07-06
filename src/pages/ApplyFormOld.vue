78d33efe6f0cc507a586b7cb317a325656829d13
manwjc <manwjc@163.com>
2018-07-06 23:08:49 +0800

    老用户流程

--- src/pages/ApplyFormOld.vue
+++ src/pages/ApplyFormOld.vue
@@ -7,21 +7,23 @@
 			<div class="form_box">
 				<form id="FormBox" method="post">
 					<p class="displaybox">
-						<label class="label_name" for="password">手机</label><span>：</span>
-						<input class="input_normal boxflex01" type="text" name="password">
+						<label class="label_name" for="mobile">手机</label><span>：</span>
+						<input class="input_normal boxflex01" type="text" name="mobile" v-model="mobile">
 					</p>
 					<p class="displaybox">
-						<label class="label_name" for="password">验证码</label><span>：</span>
-						<input class="input_normal boxflex01" type="text" name="password">
-					</p>
-					<p class="mb0">
-						<label class="label_name" for="password"></label>
-						<span class="f18 grey mleft10">验证码</span>
+						<label class="label_name" for="code">验证码</label><span>：</span>
+						<input class="input_normal boxflex01 wp100" type="text" name="code" v-model="code">
+						<label class="label_name mleft10 grey" @click="getCode">验证码</label>
 					</p>
 					<p class="displaybox">
+						<label class="label_name" for="userAge">年龄</label><span>：</span>
+						<input class="input_normal boxflex01" type="text" name="userAge" v-model="userAge">
+					</p>
+					<div class="displaybox f30">
 						<label class="label_name" for="password">是否在读学员</label><span>：</span>
-						<input class="" type="radio" name="radio">
-					</p>
+						<div><input class="" type="radio" name="radio" /> 是</div>
+						<div><input class="" type="radio" name="radio" /> 否</div>
+					</div>
 				</form>
 			</div>
 		</div>
@@ -32,6 +34,7 @@
 
 <script>
 	import Vue from 'vue'
+	import Valid from '../js/common/validate'
 	import showMessage from 'vue-show-message'
 	
 	Vue.use(showMessage, {
@@ -42,32 +45,101 @@
 		name: 'applyFormNew',
 		data() {
 			return {
-				productList: {},
-				productNum: 1,
-				curtabNum: 0,	
+				partName: '',
+				partNum: '',
+				userName: '',
+				mobile: '',
+				code: '',
+				userAge: '',
 			}
 		},
 		mounted() {
-
+			console.log(Valid)
 		},
 		methods: { 
 			submitForm() {
-				this.$axios({
+				let self = this;
+				let validateMsg = self.validateResult();
+				if(validateMsg){
+					self.$showMsg(validateMsg);
+					return;
+				}
+
+				self.$axios({
 					method:"post",
-					url:"/api/haveUser",
+					url:"/wxApi/signUp",
 					data:{
-						name:this.productNum,
-						password:this.curtabNum
+						readRoomId:1,
+						readRoomName:self.partName,
+						termNum:self.partNum,
+						signName:self.userName,
+						signMobile:self.mobile,
+						age:self.userAge,
+						validateCode:self.code
 					}
+				})
+				.then((res)=>{
+					console.log(res.data);
+					self.$router.push({name:"nameListOld",query:{id:1}})
+				})
+				.catch((error) => {
+					console.log(error)
+				})
+			},
+			validateResult:function () {
+                var self = this,
+                    names = [
+                        {name:'mobile', reg:Valid.validate.validateMobile},
+                        {name:'code'}
+                    ],
+                    message;
+                var messages = {
+                    mobile: {require: '手机号不能为空', regex: '手机号不合法，请重新输入'},
+                    code: {require: '验证码不能为空', regex: '验证码不正确，请重新输入'}
+                };
+
+                for (var i = 0; i < names.length; i++) {
+                    var item = names[i];
+                    var name = item.name;
+                    if (!self[name]) {
+                        message = messages[name].require;
+                        break;
+                    }
+                    if (item.reg&&!item.reg(self[name])) {
+                        message = messages[name].regex;
+                        break;
+                    }
+                }
+                return message;
+			},
+			getCode: function(){
+				let self = this;
+				let mobile = self.mobile,
+					dataParams = {
+						mobile:mobile
+					};
+
+				if(!mobile){
+					self.$showMsg('请输入手机号');
+					return;
+				}
+				if(!Valid.validate.validateMobile(mobile)){
+					self.$showMsg('手机号不合法，请重新输入');
+					return;
+				}
+
+				self.$showMsg('验证码已发送');
+				self.$axios.get('/wxApi/sendValidateCode', {
+					params: dataParams
 				})
 				.then((res)=>{
 					console.log(res.data);
 				})
 				.catch((error) => {
+					self.$showMsg('网络较慢，请稍候重试');
 					console.log(error)
-					this.$router.push({name:"nameListNew",query:{id:1}})
 				})
-			},
+			}
 
 		}
 	}
@@ -78,7 +150,8 @@
 	body {
 		 text-align: left;
 	}
-	.form_list{  background: url(../assets/images/bg_repeat.jpg) repeat-y; background-size: 100% 100%;}
+	.form_list{ background: url(../assets/images/bg_repeat.jpg) repeat-y; background-size: 100% 100%;}
 	.form_box{ padding: 0 0.7rem 0 0.5rem; margin: 0 auto;}
 	.form_box p{ margin-bottom: 15px; overflow: hidden;}
+	input[type=radio]{ width: 0.3rem; height: 0.3rem; vertical-align: -1px;}
 </style>
--- src/pages/NameListOld.vue
+++ src/pages/NameListOld.vue
@@ -1,29 +1,90 @@
 <template>
-	<div>
-		<p>登录页面</p>
-		<div>
-			<form action="/login" method="post">
-				<p>
-					<label for="username">用户名</label>
-					<input type="text" id="username" name="username" placeholder="用户名">
-				</p>
-				<p>
-					<label for="password">密码 : </label>
-					<input type="text" id="password" name="password" placeholder="密码">
-				</p>
-				<p>
-					<input type="submit" value="登录">
-				</p>
-			</form>
+	<div class="relative">
+		<img src="../assets/images/p_a_01.jpg">
+		<img src="../assets/images/p_a_02.jpg">
+		<img src="../assets/images/p_b_03.jpg">
+		<div class="form_list">
+			<div class="form_box f30">
+				<!-- <div class="displaybox mb10" v-for="(item, index) in nameList">
+					<span>{{index+1}}.</span>
+					<div class="head_img mleft5"><img :src="item.pic"></div>
+					<div class="boxflex01 mleft5">{{item.name}}</div>
+					<span class="w50 right">已报名</span>
+				</div> -->
+				<div class="displaybox mb10" v-for="(item, index) in nameData">
+					<span>{{index+1}}.</span>
+					<div class="head_img mleft5"><img :src="item.pic"></div>
+					<div class="boxflex01 mleft5">{{item.userName}}</div>
+					<span class="w50 right">已报名</span>
+				</div>
+			</div>
 		</div>
+		<div><img src="../assets/images/p_d_06.jpg"></div>
+		<img src="../assets/images/p_a_07.jpg">
 	</div>
 </template>
 
 <script>
+	import Vue from 'vue'
+	import showMessage from 'vue-show-message'
+	
+	Vue.use(showMessage, {
+		duration: 2000
+	})
+
+	export default {
+		name: 'applyFormNew',
+		data() {
+			return {
+				nameList: [
+						{
+							pic: '../assets/images/head_img.jpg',
+							name: 'jerry',
+						},
+						{
+							pic: './dist/head_img.jpg',
+							name: 'jerry',
+						},
+						{
+							pic: '/dist/head_img.jpg',
+							name: 'jerry',
+						},
+						{
+							pic: 'dist/head_img.jpg',
+							name: 'jerry',
+						}
+					],
+				nameData: null
+			}
+		},
+		mounted() {
+			this.getNameList();
+
+		},
+		methods: { 
+			getNameList() {
+				let self = this;
+
+				self.$axios.get('/login').then(
+					self.$axios.get('/wxApi/recommendSignUp', {
+						params: {}
+					})
+					.then((res)=>{
+						console.log(res.data);
+						self.nameData = res.data.data;
+					})
+				)
+			},
+		}
+	}
 </script>
 
-<style>
+<style scope>
+	html, body{height:100%; min-height: 100%; color: #79d1d5;}
 	body {
-		text-align: center;
+		 text-align: left;
 	}
+	.form_list{ background: url(../assets/images/bg_repeat.jpg) repeat-y; background-size: 100% 100%;}
+	.form_box{ padding: 0 0.7rem 0 0.5rem; margin: 0 auto;}
+	.form_box p{ margin-bottom: 15px;}
 </style>
