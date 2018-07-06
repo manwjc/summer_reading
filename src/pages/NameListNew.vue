8c884975b4f22b411115a8eedf27ce6cb947ca7f
manwjc <manwjc@163.com>
2018-07-07 07:33:36 +0800

    增加支付方法

--- src/pages/NameListNew.vue
+++ src/pages/NameListNew.vue
@@ -75,16 +75,82 @@
 					})
 				)
 			},
+			toPay() {
+			
+				var ajaxURL= "../order/submitOrderMulti.do";       
+				jQuery.ajax({
+					url: ajaxURL,//ajax调用微信统一接口获取prepayId
+					cache: false,
+					dataType:"json",
+					async:false,
+					data: dataParams + "&shouldPayAmt=" + shouldPayAmt,
+					success:function(data) {
+						var obj = jQuery.parseJSON(data);
+						if(obj.status !=undefined && "0000"!=obj.status){//提交订单失败
+							issubmited = false;
+							$('.btn-check-pay').removeClass('bggrey01').addClass('bgred');
+							return;
+						}
+						
+						if(shouldPayAmt<0.000001){//100%用券支付成功
+							history.replaceState({}, '我的订单', '../order/qryOrderList.do'); //这一行会导致android支付失败，因为修改了支付授权目录，不是./cart开头
+							window.location.href = "../order/viewOrderDetail.do?orderId="+obj.orderId+"&isNeedSendMsg=1";
+							return false;
+						}
+					
+						WeixinJSBridge.invoke('getBrandWCPayRequest',{  
+							"appId":obj.appId,                 //公众号名称，由商户传入  
+							"timeStamp":obj.timeStamp,          //时间戳，自 1970 年以来的秒数  
+							"nonceStr":obj.nonceStr,         //随机串  
+							"package":obj.package,      //prepay_id=*** 
+							"signType":obj.signType,        //微信签名方式:MD5  
+							"paySign":obj.paySign           //微信签名  
+							},function(res){      
+								//alert(res.err_msg); // get_brand_wcpay_request:ok
+							if(res.err_msg == "get_brand_wcpay_request:ok" ) {  
+								history.replaceState({}, '我的订单', '../order/qryOrderList.do'); //这一行会导致android支付失败，因为修改了支付授权目录，不是./cart开头，需在支付成功后设置
+								window.location.href = "../order/viewOrderDetail.do?orderId="+obj.orderId+"&isNeedSendMsg=1";
+							}else{  
+								alert("付款失败，您可查看订单，继续支付");
+								//alert("详细错误" + res.err_msg);
+								window.location.href="../order/qryOrderList.do";     
+							//当失败后，继续跳转该支付页面让用户可以继续付款，特别注意不能直接调转jsp，不然会报system:access_denied 
+							}  
+					});
+					issubmited = false;
+						$('.btn-check-pay').removeClass('bggrey01').addClass('bgred');
+						
+					},
+					error: function(data) {
+						alert("确认付款失败，请稍后再试");
+						issubmited = false;
+						$('.btn-check-pay').removeClass('bggrey01').addClass('bgred');
+					}
+				});
+			}
 		}
 	}
 </script>
 
 <style scope>
-	html, body{height:100%; min-height: 100%; color: #79d1d5;}
-	body {
-		 text-align: left;
-	}
-	.form_list{ background: url(../assets/images/bg_repeat.jpg) repeat-y; background-size: 100% 100%;}
-	.form_box{ padding: 0 0.7rem 0 0.5rem; margin: 0 auto;}
-	.form_box p{ margin-bottom: 15px;}
+html,
+body {
+  height: 100%;
+  min-height: 100%;
+  color: #79d1d5;
+}
+body {
+  text-align: left;
+}
+.form_list {
+  background: url(../assets/images/bg_repeat.jpg) repeat-y;
+  background-size: 100% 100%;
+}
+.form_box {
+  padding: 0 0.7rem 0 0.5rem;
+  margin: 0 auto;
+}
+.form_box p {
+  margin-bottom: 15px;
+}
 </style>
